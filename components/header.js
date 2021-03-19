dabae276fd5c93272611b72e870e93b8066768e2
Justus Witmer <justus.witmer@gmail.com>
2021-03-19 06:42:23 -0500

    built expectations section

--- components/header.js
+++ components/header.js
@@ -1,12 +1,13 @@
 import Link from 'next/link'
 import style from './header.module.css';
+import ICE from '../public/images/ICE.svg';
 
 export default function Header() {
   return (
     <div className={style.container}>
       <div>
-        <div>
-          ICE
+        <div className={style.logo}>
+          <ICE width={100} height={100} />
         </div>
         <div className={style.logoDescription}>
           Iserman Contracting and Excavating
--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,48 @@ export default function Home() {
       <Head>
         <title>ICE</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Dhurjati&display=swap" rel="stylesheet" />
       </Head>
       <Header />
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Justus! Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.expectContainerRotate}>
+          <div className={styles.expectContainer}>
+            <h2>What can you expect from ICE?</h2>
+            <div className={styles.expectDetailContain}>
+              <div className={styles.expectDetail}>
+                <h3>Reliability</h3>
+                <p>We do what we say we are going to do to earn our customers trust. Every Time!</p>
+              </div>
+              <div className={styles.expectDetail}>
+                <h3>Professional Service</h3>
+                <p>It matters to us that you get the quality service that you deserve.</p>
+              </div>
+              <div className={styles.expectDetail}>
+                <h3>Hassle-Free Quotes</h3>
+                <p>Get a customized quote in minutes and schedule service immediately.</p>
+              </div>
+              <div className={styles.expectDetail}>
+                <h3>Transparency</h3>
+                <p>Surprises, in this business are not a good thing.</p>
+              </div>
+              <div className={styles.expectDetail}>
+                <h3>Something</h3>
+                <p>More somethings.</p>
+              </div>
+              <div className={styles.expectDetail}>
+                <h3>Honest Pricing</h3>
+                <p>Competitive rates with no hidden costs.</p>
+              </div>
+              <div className={styles.expectDetail}>
+                <h3>Customer Experience Guarantee</h3>
+                <p>We care about every aspect of our service. If you aren't happy, we will make it right!</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
       <Footer />
     </div >
