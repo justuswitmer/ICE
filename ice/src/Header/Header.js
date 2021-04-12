469d16a6e3b9a51c59fe92cf1cde2c5e669e84c9
Justus Witmer <justus.witmer@gmail.com>
2021-04-11 22:03:58 -0500

    intro done

--- ice/src/Header/Header.js
+++ ice/src/Header/Header.js
@@ -5,11 +5,11 @@ import './header.css';
 function Header(props) {
   return (
     <div className='header'>
-      <div className='header__a' onClick={() => props.history.push('/')}>
-        <div className='header__a__a'><h2>ICE</h2></div>
-        <div className='header__a__b'><p>Iserman</p> <p>Contracting & Excavating</p></div>
+      <div className='header__b567' onClick={() => props.history.push('/')}>
+        <div className='header__b__b269'><h2>ICE</h2></div>
+        <div className='header__b__b431'><p>Iserman</p> <p>Contracting & Excavating</p></div>
       </div>
-      <div className='header__b'>
+      <div className='header__b284'>
         <p onClick={() => props.history.push('/services')}>Services</p>
         <p onClick={() => props.history.push('/quote')}>Get a Quote</p>
         <p onClick={() => props.history.push('/login')}>Log in</p>
--- ice/src/Home/Home.js
+++ ice/src/Home/Home.js
@@ -1,7 +1,34 @@
+import './Home.css';
+import rightArrow from '../assets/images/rightArrow.png';
+
 function Home() {
+
+  let title = "Your heavy lifting ends here!";
+  let description = "Are you looking for industry leading contractors that are reliable, quality, and fair priced? Well I guess we couldn't hide forever. See what ICE Contractors can do for your projects with a 2 minute quote!";
+  let quoteDescription = "Get a quote in under 2 minutes for your excavating, contracting and snow removal needs. Enter your zip code.";
+
   return (
-    <div>
-      In Home
+    <div className='home'>
+      <div className='home__b491'>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <div className='home__b__b516'>
+          <p className='home__b__b--e900'>{quoteDescription}</p>
+          <div className='home__b__b__b912'>
+            <input className='home__b__b__b--e582' value='Enter your zipcode' />
+            <button className='home__b__b__b--e368'>Get my Quote <img className='home__b__b__b--e--e440' src={rightArrow} alt='' /></button>
+          </div>
+        </div>
+      </div>
+      <div className='home__b383'>
+        Snow removal & Excavating
+      </div>
+      <div className='home__b226'>
+        Expectations
+      </div>
+      <div className='home_b808'>
+        Reviews
+      </div>
     </div>
   );
 }
